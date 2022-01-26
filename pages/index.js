56b56714b19271bcd74baf0d30db212eb1ac1603
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-01-25 22:32:24 -0300

    Testes in svg

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import logo from '../assets/svg/logo.svg';
 import { AnimationBox } from '../src/components/zamiAnimations/index';
 import appConfig from '../config.json';
+import Wave from '../assets/svg/waveSvg';
 
 // xs: 'linear-gradient(#21296C, #0032F4)',
 // md: 'linear-gradient(#21296C, #161853)',
@@ -11,16 +12,38 @@ import appConfig from '../config.json';
 function HomePage() {
     return (
         <>
-            <Box
+            {/* <Box
             styleSheet={{
                 backgroundColor: {
-                lg: appConfig.theme.colors.primary['700'],
+                xs: appConfig.theme.colors.neutrals['000'],
                 },
             }}
             >
-            </Box>
+            </Box> */}
+           
+            {/* <div className='bg-container'> */}
+                <Wave 
+                styleSheet={{
+                    position: 'absolute',
+                    bottom: '0',
+                }} />
+                {/* <div className='quadrado'></div> */}
+            {/* </div> */}
+
+            <style jsx>{`
+                .bg-container {
+                    background-color: #21296C;
+                    position: relative;
+                    height: 50vh;
+                }
+                .quadrado {
+                    height: 50vh;
+                    width: 100%;
+                    background-color: #21296C;
+                }
+            `}</style>
         </>
     )
 }
 
-export default HomePage
+export default HomePage 
